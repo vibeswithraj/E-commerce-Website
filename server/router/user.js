0566e752ed8e4639c183ca959d964c1940e72c4a
vibeswithraj <rajbhai82581@gmail.com>
2025-04-28 18:23:42 +0530

    sdsg

--- server/router/user.js
+++ server/router/user.js
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 const router = express.Router();
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import dotenv from "dotenv";
-dotenv.config({ path: "./config.env" });
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config({ path: './config.env' });
 import {
   login,
   logout,
@@ -13,14 +13,14 @@ import {
   orders,
   address,
   profileImage,
-} from "../controller/user.js";
-import { checkAuth } from "../helpers/userAuth.js";
+} from '../controller/user.js';
+import { checkAuth } from '../helpers/userAuth.js';
 
 router.use(
   cors({
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    //origin: process.env.FRONTEND_URI,
-    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    origin: process.env.FRONTEND_URI,
+    // origin: true,
     credentials: true,
   })
 );
@@ -29,15 +29,15 @@ router.use(cookieParser());
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
-router.get("/me", checkAuth, myProfile);
+router.get('/me', checkAuth, myProfile);
 
-router.post("/register", register);
-router.post("/login", login);
-router.post("/updatePass", updatePass);
-router.post("/profileImage", checkAuth, profileImage);
-router.get("/logout", logout);
+router.post('/register', register);
+router.post('/login', login);
+router.post('/updatePass', updatePass);
+router.post('/profileImage', checkAuth, profileImage);
+router.get('/logout', logout);
 
-router.get("/orders", checkAuth, orders);
-router.get("/address", checkAuth, address);
+router.get('/orders', checkAuth, orders);
+router.get('/address', checkAuth, address);
 
 export default router;
